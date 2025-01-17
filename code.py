060ab1489707ba2cd2cb67dc11706253675841ad
Miin-branch-prog-c-python <tienminh.be@hotmail.com>
2025-01-17 15:57:28 +0100

    code.py

---
+++ code.py
@@ -0,0 +1,5 @@
+########################################
+# Author: miin-branch-prog-c-python
+# Date of creation: 15/02/2025
+# Credits: see line 5 to line .
+########################################
